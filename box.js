dba6a80f42008890adda17b56b6db3383866413a
kanikachandak <64009261+kanikachandak@users.noreply.github.com>
2020-12-26 23:25:20 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,43 @@
+class Box
+{
+    constructor(x, y)
+    {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        };
+        this.body = Bodies.rectangle(x, y, 40, 40, options);
+        this.width = 40;
+        this.height = 40;
+        this.Visibility=255;
+        this.img=loadImage("box.png");
+        World.add(world,this.body);
+    }
+    display()
+    {
+        if(this.body.speed<3)
+        {
+            var pos =this.body.position;
+            var angle = this.body.angle;
+            strokeWeight(3);
+            stroke("brown");
+            fill("pink");
+            push();
+            translate(pos.x, pos.y);
+            rotate(angle);
+            imageMode(CENTER);
+            image(this.img, 0, 0, this.width, this.height); 
+            pop(); 
+        }
+        else
+        {
+            World.remove(world,this.body);
+            push();
+            this.Visibility=this.Visibility-50;
+            tint(255,this.Visibility);
+            image(this.img, this.body.position.x, this.body.position.y, 40, 40);
+            pop();
+        }
+    }
+}
